79b3e3b54fde3de6742634de3fe2cd29dd44544a
AlexSaxena <saxenaalex@gmail.com>
2019-09-23 16:33:13 +0200

    configures shoulda matchers

--- config/application.rb
+++ config/application.rb
@@ -21,3 +21,10 @@ module RailsDemo
     config.generators.system_tests = nil
   end
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails 
+  end
+end
