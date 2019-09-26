5a82324eba1d8c48568c7a9f9fe94493e719c824
AlexSaxena <saxenaalex@gmail.com>
2019-09-26 19:14:13 +0200

    Adds step definitions in basic steps file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,17 +7,22 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    
     def new
+        @article = Article.new
     end
-
+    
     def create
         @article = Article.new(article_params)
-       
-        @article.save
+    
+        if @article.save
         redirect_to @article
-      end
-       
-      private
+        else
+        render 'new'
+        end
+    end
+
+    private
     def article_params
         params.require(:article).permit(:title, :text)
     end
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -2,13 +2,13 @@ When("I am on the landing page") do
     visit root_path
   end
   
-  Then("I should see {string}") do |content|
-    expect(page).to have_content content
-  end
+Then("I should see {string}") do |content|
+  expect(page).to have_content content
+end
 
-  Given("the following articles exist") do |table|
-    table.hashes.each do |article|
-        Article.create!(article)
+Given("the following articles exist") do |table|
+  table.hashes.each do |article|
+    Article.create!(article)
   end
 end
   
@@ -16,18 +16,15 @@ Given("I visit the {string} page") do |string|
   visit root_path
 end
 
-When("I click {string} link") do |string|
+When("I click {string} button/link") do |string|
  click_on string
 end
 
 When("I fill in {string} with {string}") do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When("I click {string} button") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in string, :with => string2
 end
 
 Then("I should be on {string} page") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  article = Article.find_by(title: string)
+  expect(current_path).to eq article_path(article)
 end
