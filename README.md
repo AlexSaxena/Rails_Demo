96bae1e2094f60b104362546452304ed882298a1
AlexSaxena <saxenaalex@gmail.com>
2019-09-26 15:10:51 +0200

    Updates Readme with a user story, adds new step def for creating article, Adds new features for create article

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Create an applciation which will display a list of articles on the landing page.
     -Test driven
         -Feature tests
         -Unit tests
+
+
+## User Stories
+
+As a publisher
+In order to add relevant content to my news service
+I would like to be able to create articles
+Here is a starting feature file for the create functionality.
+
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -12,3 +12,22 @@ When("I am on the landing page") do
   end
 end
   
+Given("I visit the {string} page") do |string|
+  visit root_path
+end
+
+When("I click {string} link") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I fill in {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click {string} button") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be on {string} page") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
